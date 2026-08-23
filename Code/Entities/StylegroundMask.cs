e5e8e6b68c07b47b5a79d6e8f5ccb8d74edddc47
karl2883 <karl.schomerus2@gmail.com>
2026-08-23 20:52:11 +0200

    actually move it to the outer loop

--- Code/Entities/StylegroundMask.cs
+++ Code/Entities/StylegroundMask.cs
@@ -421,8 +421,8 @@ public class StylegroundMaskRenderer : Renderer {
         if (batchMasks.Any()) {
             Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone, null, level.Camera.Matrix);
             foreach (var mask in batchMasks) {
+                var slices = mask.GetMaskSlices();
                 foreach (var tag in mask.RenderTags) {
-                    var slices = mask.GetMaskSlices();
                     if (bufferDict.TryGetValue(tag, out var buffer)) {
                         foreach (var slice in slices) {
                             Draw.SpriteBatch.Draw(buffer, slice.Position, slice.Source, Color.White * slice.GetValue(mask.AlphaFrom, mask.AlphaTo));
